48a6b0f256f21e70e8e215e0c1a01a58c51c6c8a
Liuyubo <yubo.liu@17zuoye.com>
2018-10-09 16:41:57 +0800

    集合

--- SwiftDemo/FirstViewController.swift
+++ SwiftDemo/FirstViewController.swift
@@ -39,6 +39,7 @@ class FirstViewController: UIViewController {
 //        let txt = msg + str1!  // runtime error
         
         stringIndex()
+        setDemo()
     }
     
     func isObjEqual<T:Equatable>(x: T,y: T) -> Bool {
@@ -52,4 +53,23 @@ class FirstViewController: UIViewController {
         print(name.endIndex)
     }
     
+    func setDemo() {
+        var array = [Int]()
+        var array1:[Int] = [2,4,6]
+        var array2 = Array(repeating: 4, count: 5)
+        for item in array1 {
+            print(item)
+        }
+        
+        var setArray = Set<String>()
+        var favoriteGenres: Set<String> = ["Rock", "Classical", "Hip hop"]
+        
+        var dic = [Int: String]()
+        // namesOfIntegers 现在包含一个键值对
+        dic[16] = "sixteen"
+        // namesOfIntegers 又成为了一个 [Int: String] 类型的空字典
+        dic = [:]
+        
+        var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+    }
 }
