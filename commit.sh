dd33fc4539b92646e8c32087cb1c5fee11794aae
yubo liu <liuyb@iclouddoor.com>
2019-09-13 01:30:37 +0800

    not

--- commit.sh
+++ commit.sh
@@ -4,4 +4,8 @@ git add .
 git commit -m $1
 echo $1
 echo "----end----"
-git push origin master
+echo "----start push"
+result=`git branch | grep "*"` # 获取分支名
+curBranch=${result:2} # 去除多余的*
+echo "当前分支 -- ${curBranch}"
+git push origin $curBranch
